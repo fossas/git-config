5fb2fadf7f2aa422514b5c43bc0bd395e1e2fcd0
Fernando Freire <dogonthehorizon@gmail.com>
2018-02-10 10:11:21 -0800

    Add remaining parser docstrings
    Also did some refactoring to simplify the parser. Added a makefile to
    make builds a little more consistent (and more importantly make sure
    that we're always comitting a fresh cabal file.)

---
+++ Makefile
@@ -0,0 +1,34 @@
+.PHONY: hlint_install, hlint, hlint_apply_refact, hlint_refactor
+.PHONY: stylish_haskell_install, stylish_haskell_check, clean
+
+STACK_BUILD := stack build
+
+build: clean
+	@$(STACK_BUILD)
+
+watch: build
+	@$(STACK_BUILD) --test --coverage --haddock --file-watch
+
+clean:
+	@stack clean && rm *.cabal
+
+# The following tasks cribbed from: https://lwm.github.io/haskell-static/
+hlint_install:
+	@stack install hlint
+
+hlint: hlint_install
+	@hlint src/
+
+hlint_apply_refact: hlint_install
+	@stack install apply-refact
+
+HLINT=hlint --refactor --refactor-options -i {} \;
+hlint_refactor: hlint_apply_refact
+	@find src/ -name "*.hs" -exec $(HLINT)
+
+stylish_haskell_install:
+	@stack install stylish-haskell
+
+STYLISH=stylish-haskell -i {} \;
+stylish_haskell_check: stylish_haskell_install
+	@find src/ -name "*.hs" -exec $(STYLISH) && git diff --exit-code
--- src/Text/GitConfig/Parser.hs
+++ src/Text/GitConfig/Parser.hs
@@ -1,6 +1,45 @@
+{-|
+Module      : Text.GitConfig.Parser
+Description : Parser-combinators for Git configuration files.
+Copyright   : (c) Fernando Freire, 2018
+License     : BSD3
+Maintainer  : dogonthehorizon@gmail.com
+Stability   : experimental
+
+This module provides parser-combinators for Git configuration files.
+
+It attempts to follow the syntax for Git configuration files outlined in
+this document:
+
+https://git-scm.com/docs/git-config/2.16.0#_syntax
+
+One notable omission is that no legacy compatability is explicitly provided
+(e.g. dot-separated subsection names are not guaranteed to parse correctly).
+-}
+
 {-# LANGUAGE TypeFamilies #-}
 
-module Text.GitConfig.Parser where
+module Text.GitConfig.Parser (
+  -- * Types
+  Parser,
+  Section(..),
+  GitConfig,
+  -- * Lexer
+  spaceConsumer,
+  lexeme,
+  symbol,
+  brackets,
+  quotes,
+  escSeq,
+  -- * Parser
+  sectionName,
+  sectionHeader,
+  variableName,
+  variableValue,
+  mapping,
+  section,
+  config
+) where
 
 import           Control.Applicative        (empty)
 import           Control.Monad              (void)
@@ -10,52 +49,44 @@ import qualified Data.HashMap.Strict        as M
 import           Data.Text                  (Text)
 import qualified Data.Text                  as T
 import           Data.Void                  (Void)
-import           Text.Megaparsec            (MonadParsec, ParseError, Parsec,
-                                             Token, between, many,
-                                             sepBy, some, try, (<|>), eof, (<?>))
-import           Text.Megaparsec.Char       (alphaNumChar, printChar, letterChar,
-                                             space1, char, eol, satisfy)
+import           Text.Megaparsec            (Parsec, between, eof, many, sepBy,
+                                             some, try, (<?>), (<|>))
+import           Text.Megaparsec.Char       (alphaNumChar, char, eol,
+                                             letterChar, printChar, satisfy,
+                                             space1)
 import qualified Text.Megaparsec.Char.Lexer as Lexer
 
 type Parser = Parsec Void Text
-type GitConfigError = ParseError (Token Text) Void
 
--- FIXME I'm not satisfied with this type, it doesn't capture
---       a Git configuration's hierarchy properly. Rose tree could work,
---       but there may be a better data  structure for capturing this.
 data Section = Section [Text] (HashMap Text Text)
-  deriving (Show)
+  deriving (Eq, Show)
+
+type GitConfig = [Section]
 
 -- | Whitespace consumer for this parser.
+--
+-- Whitespace is considered to be any space character
+-- (including carriage return) as well as line comments starting with `#` and
+-- `;` .
 spaceConsumer :: Parser ()
 spaceConsumer = Lexer.space space1 lineComment empty
   where lineComment = Lexer.skipLineComment "#" <|> Lexer.skipLineComment ";"
 
--- | A lexeme for this parser. Consumes trailing whitespace.
+-- | A lexeme for this parser.
 lexeme :: Parser a -> Parser a
 lexeme = Lexer.lexeme spaceConsumer
 
--- | A symbol for this parser. Consumes trailing whitespace.
+-- | A symbol for this parser.
 symbol :: Text -> Parser Text
 symbol = Lexer.symbol spaceConsumer
 
--- | Combinator returning the combinator between brackets.
+-- | Return a parser in between brackets.
 brackets :: Parser a -> Parser a
 brackets = between (symbol "[") (symbol "]")
 
--- | Combinator returning the combinator between quotes.
+-- | Return a parser in between quotes.
 quotes :: Parser a -> Parser a
 quotes = between (symbol "\"") (symbol "\"")
-
--- | Parser for the `.` character.
-dot :: (MonadParsec e s m, Token s ~ Char) => m (Token s)
-dot = char '.'
-{-# INLINE dot #-}
-
--- | Parser for the `-` character.
-dash :: (MonadParsec e s m, Token s ~ Char) => m (Token s)
-dash = char '-'
-{-# INLINE dash #-}
 
 -- | Parser for escape sequences.
 escSeq :: Parser Char
@@ -77,13 +108,10 @@ escSeq = char '\\' *> escSeqChar
 -- characters, - and . are allowed in section names. Sections can be further
 -- divided into subsections. To begin a subsection put its name in double
 -- quotes, separated by space from the section name, in the section header.
---
--- From the git 2.16 docs found here:
--- https://git-scm.com/docs/git-config/2.16.0#_syntax
 sectionName :: Parser [Text]
 sectionName = (section <|> subSection) `sepBy` spaceConsumer
   where
-    sectionChar = alphaNumChar <|> dot <|> dash
+    sectionChar = alphaNumChar <|> char '.' <|> char '-'
     section     = fmap T.pack . some $ sectionChar
     subSection  = fmap T.pack . quotes . many $
       escSeq <|> satisfy (\x -> x `notElem` ['"', '\\'])
@@ -92,32 +120,26 @@ sectionName = (section <|> subSection) `sepBy` spaceConsumer
 --
 -- A section begins with the name of the section in square brackets and
 -- continues until the next section begins.
---
--- From the git 2.16 docs found here:
--- https://git-scm.com/docs/git-config/2.16.0#_syntax
 sectionHeader :: Parser [Text]
 sectionHeader = brackets sectionName
 
 -- | Parse a variable name.
 --
---  The variable names are case-insensitive, allow only alphanumeri
+--  The variable names are case-insensitive, allow only alphanumeric
 --  characters and -, and must start with an alphabetic character.
---
--- From the git 2.16 docs found here:
--- https://git-scm.com/docs/git-config/2.16.0#_syntax
 variableName :: Parser Text
 variableName = fmap (T.toLower . T.pack) . lexeme . try $ p
   where
-    p = (:) <$> letterChar <*> many (alphaNumChar <|> dash)
+    p = (:) <$> letterChar <*> many (alphaNumChar <|> char '-')
 
 -- | Parse a variable value.
 variableValue :: Parser Text
 variableValue = T.pack <$> between spaceConsumer eol (many printChar)
 
--- | Parse a mapping of 'Text' key to value.
+-- | Parse a tuple of 'Text' key and value.
 --
 --  in the form name = value (or just name, which is a short-hand to say that
---  the variable is the boolean "true").
+--  the variable is the boolean "true"). -- FIXME parse boolean true
 --
 --  A line that defines a value can be continued to the next line by ending
 --  it with a \; the backquote and the end-of-line are stripped. Leading
@@ -141,5 +163,5 @@ section = do
   return $ Section header (M.fromList sectionValues)
 
 -- | Parse a complete git config.
-config :: Parser [Section]
+config :: Parser GitConfig
 config = between spaceConsumer eof $ many section
